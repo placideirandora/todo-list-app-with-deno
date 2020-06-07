5087087e77cf4536fa07ed611e30b01d6aaff69b
Placide IRANDORA <placideirandora@gmail.com>
2020-06-07 20:06:23 +0200

    Improve the way the App listens to the Server

--- server.ts
+++ server.ts
@@ -1,17 +1,22 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
 
 const app = new Application();
 const router = new Router();
 const port: number = 8080;
 
-router.get("/", ({ response }: { response: any }) => {
+router.get('/', ({ response }: { response: any }) => {
   response.body = {
-    message: "hello deno, nahasesekaye kbx!",
+    message: 'hello deno, nahasesekaye kbx!',
   };
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log('running on port ', port);
+app.addEventListener('listen', ({ secure, hostname, port }) => {
+  const protocol = secure ? 'https://' : 'http://';
+  const url = `${protocol}${hostname ?? 'localhost'}:${port}`;
+  console.log(`Listening on Port: ${port}`);
+});
+
 await app.listen({ port });
