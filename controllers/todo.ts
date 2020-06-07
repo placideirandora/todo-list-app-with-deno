7df22bf8da4574518fbb1365ebb7ad67677db4ab
Placide IRANDORA <placideirandora@gmail.com>
2020-06-07 21:15:35 +0200

    Implement a Controller for Retrieving All Todos

--- controllers/todo.ts
+++ controllers/todo.ts
@@ -1,7 +1,18 @@
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
+
+import Todo from '../interfaces/Todo.ts';
+import todos from '../stubs/todos.ts';
+
 export default {
-  getAllTodos: () => {},
-  createTodo: async () => {},
-  getTodoById: () => {},
-  updateTodoById: async () => {},
-  deleteTodoById: () => {},
+  /**
+   * @description Get all todos
+   * @route GET /todos
+   */
+  getAllTodos: ({ response }: { response: any }) => {
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: todos,
+    };
+  },
 };
