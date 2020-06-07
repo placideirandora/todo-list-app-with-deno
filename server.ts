c2caaa03800047493896ddc1d3025e839562ddb7
Placide IRANDORA <placideirandora@gmail.com>
2020-06-07 21:55:21 +0200

    Implement Logging for HTTP Requests

--- server.ts
+++ server.ts
@@ -3,10 +3,13 @@ import { green, yellow } from 'https://deno.land/std@0.53.0/fmt/colors.ts';
 
 import todoRouter from './routes/todo.ts';
 import notFound from './middleware/notFound.ts';
+import logger from './middleware/logger.ts';
 
 const app = new Application();
 const port: number = 8080;
 
+app.use(logger.logger);
+app.use(logger.responseTime);
 app.use(todoRouter.routes());
 app.use(todoRouter.allowedMethods());
 app.use(notFound);
