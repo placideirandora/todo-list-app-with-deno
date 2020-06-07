f09ff6c25f3e21c10f7f22d61d9841dcfdae3457
Placide IRANDORA <placideirandora@gmail.com>
2020-06-07 20:21:06 +0200

    Specify the Endpoint Routes and their corresponding empty controllers

---
+++ controllers/todo.ts
@@ -0,0 +1,7 @@
+export default {
+  getAllTodos: () => {},
+  createTodo: async () => {},
+  getTodoById: () => {},
+  updateTodoById: async () => {},
+  deleteTodoById: () => {},
+};
--- server.ts
+++ server.ts
@@ -1,23 +1,18 @@
 import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
 import { green, yellow } from 'https://deno.land/std@0.53.0/fmt/colors.ts';
 
+import todoRouter from './routes/todo.ts';
+
 const app = new Application();
-const router = new Router();
 const port: number = 8080;
 
-router.get('/', ({ response }: { response: any }) => {
-  response.body = {
-    message: 'hello deno, nahasesekaye kbx!',
-  };
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(todoRouter.routes());
+app.use(todoRouter.allowedMethods());
 
 app.addEventListener('listen', ({ secure, hostname, port }) => {
   const protocol = secure ? 'https://' : 'http://';
   const url = `${protocol}${hostname ?? 'localhost'}:${port}`;
-  console.log(`${yellow("Listening on Port:")} ${green(url)}`);
+  console.log(`${yellow('Listening on Port:')} ${green(url)}`);
 });
 
 await app.listen({ port });
