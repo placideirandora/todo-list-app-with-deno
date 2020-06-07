470102fdc7a4042491e7d75f0ca26d3a306e58e0
Placide IRANDORA <placideirandora@gmail.com>
2020-06-07 21:16:22 +0200

    Implement a Controller for Creating a Todo

--- controllers/todo.ts
+++ controllers/todo.ts
@@ -15,4 +15,41 @@ export default {
       data: todos,
     };
   },
+  /**
+   * @description Add a new todo
+   * @route POST /todos
+   */
+  createTodo: async ({
+    request,
+    response,
+  }: {
+    request: any;
+    response: any;
+  }) => {
+    const body = await request.body();
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        message: 'No data provided',
+      };
+      return;
+    }
+
+    // if everything is fine then perform
+    // operation and return todos with the
+    // new data added.
+    let newTodo: Todo = {
+      id: v4.generate(),
+      todo: body.value.todo,
+      isCompleted: false,
+    };
+
+    let data = [...todos, newTodo];
+
+    response.body = {
+      success: true,
+      data,
+    };
+  },
 };
